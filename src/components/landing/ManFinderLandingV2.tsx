d5eccf86a082c2bb398e35f203c38cf4c122d17f
nilesh2021 <nilesh.lokhande2021@gmail.com>
2026-08-18 11:28:14 +0530

    new changes

--- src/components/landing/ManFinderLandingV2.tsx
+++ src/components/landing/ManFinderLandingV2.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { ArrowUpRight, Check, Flame } from "lucide-react";
 
 import Header from "@/components/Home/Header";
