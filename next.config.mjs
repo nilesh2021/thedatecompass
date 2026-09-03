4b458cad6fa503a3afac54c878ac9a774a9d0f30
nilesh2021 <nilesh.lokhande2021@gmail.com>
2026-09-03 18:22:54 +0530

    fixed 404 error

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,42 @@ const nextConfig = {
       },
       
     ],
-  },experimental: {
+  },
+  async redirects() {
+    return [
+      {
+        source: "/offers/dreamz-ai-v2",
+        destination: "/offers/dreamz-ai-companion",
+        permanent: true,
+      },
+      {
+        source: "/category/manfinder",
+        destination: "/offers/manfinder",
+        permanent: true,
+      },
+      {
+        source: "/category/top-offers",
+        destination: "/top-offers",
+        permanent: true,
+      },
+      {
+        source: "/gonaughty-australia",
+        destination: "/offers/gonaughty-australia",
+        permanent: true,
+      },
+      {
+        source: "/offers",
+        destination: "/top-offers",
+        permanent: true,
+      },
+      {
+        source: "/offers/ManFinderLandingV2",
+        destination: "/offers/manfinder-v2",
+        permanent: true,
+      },
+    ];
+  },
+  experimental: {
     cpus: 1,
   },
 };
